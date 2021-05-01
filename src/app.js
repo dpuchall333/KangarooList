7ff821293547932d0aaad4f2433f75f0e0c578dd
dpuchall333 <danielapuchall@gmail.com>
2021-05-01 14:14:01 -0400

    change css

--- src/app.js
+++ src/app.js
@@ -338,7 +338,7 @@ app.get('/forum',(req,res)=>{
 app.get('/profile',(req,res)=>{
     if(req.session.user){
    const username = req.session.user.username;
-    res.redirect('/profile/'+username);
+    res.redirect(username);
     //res.render('profile');
     }
     else{
@@ -347,7 +347,7 @@ app.get('/profile',(req,res)=>{
 })
 
 
-app.get('/profile/:username',(req,res)=>{
+app.get('/:username',(req,res)=>{
     if(req.session.user){
     User.findOne({username: req.params.username},function(err,user){
         KangarooList.find({username:req.session.user.username},function(err,lists){
